2c81a2ad0ce452495e981b9fc04848e4628814d6
bq-t <bq-t@users.noreply.github.com>
2025-02-17 23:59:01 +0000

    deploy: da6725950268604df41528317caa30595b898cbe

---
+++ assets/Icon.stories-D95JmFzh.js
@@ -0,0 +1,61 @@
+import{_ as a}from"./Icon-CdlcyPIi.js";import"./vue.esm-bundler-CBjxpMWX.js";const p={title:"Icon",component:a,argTypes:{color:{controls:{type:"select"},options:["black","white","primary"],defaultValue:"black"},size:{controls:{type:"select"},options:["sm","md","lg"],defaultValue:"md"}}},e={args:{name:"star",color:"black",size:"lg",rounded:!0},render:o=>({components:{Icon:a},setup(){return{args:o}},template:`
+      <div :style="{ display: 'flex', alignItems: 'center' }">
+        <Icon v-bind="args" />
+        <Icon name="star" color="black" size="md" />
+        <Icon name="star" color="black" size="sm" />
+      </div>
+    `})},r={args:{name:"local-fire-department-outline",color:"primary",size:"lg",rounded:!0},render:o=>({components:{Icon:a},setup(){return{args:o}},template:`
+      <div :style="{ display: 'flex', alignItems: 'center' }">
+        <Icon v-bind="args" />
+        <Icon name="local-fire-department-outline" color="primary" size="md" />
+        <Icon name="local-fire-department-outline" color="primary" size="sm" />
+      </div>
+    `})};var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'star',
+    color: 'black',
+    size: 'lg',
+    rounded: true
+  },
+  render: args => ({
+    components: {
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div :style="{ display: 'flex', alignItems: 'center' }">
+        <Icon v-bind="args" />
+        <Icon name="star" color="black" size="md" />
+        <Icon name="star" color="black" size="sm" />
+      </div>
+    \`
+  })
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var c,l,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    name: 'local-fire-department-outline',
+    color: 'primary',
+    size: 'lg',
+    rounded: true
+  },
+  render: args => ({
+    components: {
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div :style="{ display: 'flex', alignItems: 'center' }">
+        <Icon v-bind="args" />
+        <Icon name="local-fire-department-outline" color="primary" size="md" />
+        <Icon name="local-fire-department-outline" color="primary" size="sm" />
+      </div>
+    \`
+  })
+}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const u=["DefaultIcon","PrimaryIcon"];export{e as DefaultIcon,r as PrimaryIcon,u as __namedExportsOrder,p as default};
---
+++ assets/ListItem.stories-C2A7zSRb.js
@@ -0,0 +1,45 @@
+import{a,_ as o}from"./ListItem-1aWVG0-k.js";import"./vue.esm-bundler-CBjxpMWX.js";import"./Icon-CdlcyPIi.js";import"./useRouterLink-CLGXP8Oo.js";const p={title:"ListItem",component:a},t={args:{title:"Editable item",icon:"star-outline",active:!1},render:r=>({components:{List:o,ListItem:a},setup(){return{args:r}},template:`
+      <List>
+        <ListItem v-bind="args" />
+        <ListItem
+          title="Example list item"
+          icon="favorite-outline"
+        />
+        <ListItem
+          title="Example active list item"
+          icon="favorite-outline"
+          :active="true"
+        />
+      </List>
+    `})};var e,i,s;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: 'Editable item',
+    icon: 'star-outline',
+    active: false
+  },
+  render: args => ({
+    components: {
+      List,
+      ListItem
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <List>
+        <ListItem v-bind="args" />
+        <ListItem
+          title="Example list item"
+          icon="favorite-outline"
+        />
+        <ListItem
+          title="Example active list item"
+          icon="favorite-outline"
+          :active="true"
+        />
+      </List>
+    \`
+  })
+}`,...(s=(i=t.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const u=["DefaultListItem"];export{t as DefaultListItem,u as __namedExportsOrder,p as default};
